9e7838fb814d9e92fc54bdfb6b342c405f05f0be
SuYoungKo <kosu5068@naver.com>
2022-09-15 01:15:11 +0900

    220914

---
+++ 11주차/9.14/22.09.14/order_manage_system/backend/db/index.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+	// aws ip
+	host: "3.35.152.204",
+	// mysql username
+	user: "kosu",
+	// mysql user password
+	password: "tndud0396",
+	// db name
+	database: "order_system",
+	waitForConnections: true,
+	connectionLimit: 10,
+	queueLimit: 0,
+});
+
+module.exports = { pool };
